8f7b113b8f15957815f7c0209f222ee7e5c5abad
mfuatnuroglu <mfuatnuroglu@gmail.com>
2022-10-03 10:02:14 +0300

    init

---
+++ src/Content.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+
+import appInfo from './app-info';
+import { SideNavOuterToolbar as SideNavBarLayout } from './layouts';
+import { Footer } from './components';
+import Profile from './pages/profile/profile';
+import Home from './pages/home/home';
+import DisplayData from './pages/display-data/display-data';
+
+export default function () {
+  return (
+    <SideNavBarLayout title={appInfo.title}>
+
+      <Routes>
+        <Route path="/home" element={<Home currentPath={"/home"} />} />
+        <Route path="/profile" element={<Profile currentPath={"/profile"} />} />
+        <Route path="/display-data" element={<DisplayData currentPath={"/display-data"} />} />
+        <Route
+          path="*"
+          element={<Navigate to="/home" />}
+        />
+      </Routes>
+      <Footer>
+        Copyright © 2011-2019 Developer Express Inc.
+        <br />
+        All trademarks or registered trademarks are property of their
+        respective owners.
+      </Footer>
+    </SideNavBarLayout>
+  );
+}
---
+++ src/NotAuthenticatedContent.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { SingleCard } from './layouts';
+import { LoginForm, ResetPasswordForm, ChangePasswordForm, CreateAccountForm } from './components';
+
+export default function () {
+  return (
+    <Routes>
+      <Route path='/login' element={
+        <SingleCard title="Sign In">
+          <LoginForm />
+        </SingleCard>
+      } >
+      </Route>
+      <Route path='/create-account' element={
+          <SingleCard title="Sign Up">
+            <CreateAccountForm />
+          </SingleCard>
+        }>
+      </Route>
+      <Route path='/reset-password' element={
+        <SingleCard
+          title="Reset Password"
+          description="Please enter the email address that you used to register, and we will send you an email with a link to reset your password."
+        >
+          <ResetPasswordForm />
+        </SingleCard>
+      } >
+      </Route>
+      <Route path='/change-password/:recoveryCode' element={
+        <SingleCard title="Change Password">
+          <ChangePasswordForm />
+        </SingleCard>
+      } >
+      </Route>
+      <Route element={<Navigate replace={true} to={'/login'} />}></Route>
+    </Routes>
+  );
+}
