ada4f82705dc3bff0fbaa52c647688720eed2ddb
mfuatnuroglu <mfuatnuroglu@gmail.com>
2022-10-03 10:11:00 +0300

    package-update

--- src/layouts/side-nav-inner-toolbar/side-nav-inner-toolbar.tsx
+++ src/layouts/side-nav-inner-toolbar/side-nav-inner-toolbar.tsx
@@ -1,16 +1,16 @@
-import Button from 'devextreme-react/button';
-import Drawer from 'devextreme-react/drawer';
-import ScrollView from 'devextreme-react/scroll-view';
-import Toolbar, { Item } from 'devextreme-react/toolbar';
-import React, { useState, useCallback, useRef } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { Template } from 'devextreme-react/core/template';
+import Button from "devextreme-react/button";
+import Drawer from "devextreme-react/drawer";
+import ScrollView from "devextreme-react/scroll-view";
+import Toolbar, { Item } from "devextreme-react/toolbar";
+import React, { useState, useCallback, useRef } from "react";
+import { useNavigate } from "react-router-dom";
+import { Template } from "devextreme-react/core/template";
 
-import { Header, SideNavigationMenu, Footer } from '../../components';
-import { useScreenSize } from '../../utils/media-query';
-import { useMenuPatch } from '../../utils/patches';
+import { Header, SideNavigationMenu, Footer } from "../../components";
+import { useScreenSize } from "../../utils/media-query";
+import { useMenuPatch } from "../../utils/patches";
 
-import './side-nav-inner-toolbar.scss';
+import "./side-nav-inner-toolbar.scss";
 
 interface IProps {
   title: string;
@@ -27,8 +27,8 @@ export default function ({ title, children }: IProps) {
   );
 
   const toggleMenu = useCallback(({ event }) => {
-    setMenuStatus(
-      prevMenuStatus => prevMenuStatus === MenuStatus.Closed
+    setMenuStatus((prevMenuStatus) =>
+      prevMenuStatus === MenuStatus.Closed
         ? MenuStatus.Opened
         : MenuStatus.Closed
     );
@@ -36,87 +36,91 @@ export default function ({ title, children }: IProps) {
   }, []);
 
   const temporaryOpenMenu = useCallback(() => {
-    setMenuStatus(
-      prevMenuStatus => prevMenuStatus === MenuStatus.Closed
+    setMenuStatus((prevMenuStatus) =>
+      prevMenuStatus === MenuStatus.Closed
         ? MenuStatus.TemporaryOpened
         : prevMenuStatus
     );
   }, []);
 
   const onOutsideClick = useCallback(() => {
-    setMenuStatus(
-      prevMenuStatus => prevMenuStatus !== MenuStatus.Closed && !isLarge
+    setMenuStatus((prevMenuStatus) =>
+      prevMenuStatus !== MenuStatus.Closed && !isLarge
         ? MenuStatus.Closed
         : prevMenuStatus
     );
     return true;
   }, [isLarge]);
 
-  const onNavigationChanged = useCallback(({ itemData: { path }, event, node }) => {
-    if (menuStatus === MenuStatus.Closed || !path || node.selected) {
-      event.preventDefault();
-      return;
-    }
+  const onNavigationChanged = useCallback(
+    ({ itemData: { path }, event, node }) => {
+      if (menuStatus === MenuStatus.Closed || !path || node.selected) {
+        event.preventDefault();
+        return;
+      }
 
-    navigate(path);
-    scrollViewRef.current?.instance.scrollTo(0);
+      navigate(path);
+      scrollViewRef.current?.instance.scrollTo(0);
 
-    if (!isLarge || menuStatus === MenuStatus.TemporaryOpened) {
-      setMenuStatus(MenuStatus.Closed);
-      event.stopPropagation();
-    }
-  }, [navigate, menuStatus, isLarge]);
+      if (!isLarge || menuStatus === MenuStatus.TemporaryOpened) {
+        setMenuStatus(MenuStatus.Closed);
+        event.stopPropagation();
+      }
+    },
+    [navigate, menuStatus, isLarge]
+  );
 
   return (
-    <div className={'side-nav-inner-toolbar'}>
+    <div className={"side-nav-inner-toolbar"}>
       <Drawer
-        className={['drawer', patchCssClass].join(' ')}
-        position={'before'}
+        className={["drawer", patchCssClass].join(" ")}
+        position={"before"}
         closeOnOutsideClick={onOutsideClick}
-        openedStateMode={isLarge ? 'shrink' : 'overlap'}
-        revealMode={isXSmall ? 'slide' : 'expand'}
+        openedStateMode={isLarge ? "shrink" : "overlap"}
+        revealMode={isXSmall ? "slide" : "expand"}
         minSize={isXSmall ? 0 : 60}
         maxSize={250}
         shading={isLarge ? false : true}
         opened={menuStatus === MenuStatus.Closed ? false : true}
-        template={'menu'}
+        template={"menu"}
       >
-        <div className={'container'}>
-          <Header
-            menuToggleEnabled={isXSmall}
-            toggleMenu={toggleMenu}
-          />
-          <ScrollView ref={scrollViewRef} className={'layout-body with-footer'}>
-            <div className={'content'}>
-              {React.Children.map(children, item => {
+        <div className={"container"}>
+          <Header menuToggleEnabled={isXSmall} toggleMenu={toggleMenu} />
+          <ScrollView ref={scrollViewRef} className={"layout-body with-footer"}>
+            <div className={"content"}>
+              {React.Children.map(children, (item) => {
                 return item.type !== Footer && item;
               })}
             </div>
-            <div className={'content-block'}>
-              {React.Children.map(children, item => {
+            <div className={"content-block"}>
+              {React.Children.map(children, (item) => {
                 return item.type === Footer && item;
               })}
             </div>
           </ScrollView>
         </div>
-        <Template name={'menu'}>
+        <Template name={"menu"}>
           <SideNavigationMenu
             compactMode={menuStatus === MenuStatus.Closed}
             selectedItemChanged={onNavigationChanged}
             openMenu={temporaryOpenMenu}
             onMenuReady={onMenuReady}
           >
-            <Toolbar id={'navigation-header'}>
-              {
-                !isXSmall &&
-                <Item
-                  location={'before'}
-                  cssClass={'menu-button'}
-                >
-                  <Button icon="menu" stylingMode="text" onClick={toggleMenu} />
+            <Toolbar id={"navigation-header"}>
+              {!isXSmall && (
+                <Item location={"before"} cssClass={"menu-button"}>
+                  <Button
+                    icon="menu"
+                    stylingMode="text"
+                    onClick={() => toggleMenu}
+                  />
                 </Item>
-              }
-              <Item location={'before'} cssClass={'header-title'} text={title} />
+              )}
+              <Item
+                location={"before"}
+                cssClass={"header-title"}
+                text={title}
+              />
             </Toolbar>
           </SideNavigationMenu>
         </Template>
@@ -128,5 +132,5 @@ export default function ({ title, children }: IProps) {
 const MenuStatus = {
   Closed: 1,
   Opened: 2,
-  TemporaryOpened: 3
+  TemporaryOpened: 3,
 };
