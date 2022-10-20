6ffda7886aee2405f9cf51cba630931d21d42e64
mfuatnuroglu <mfuatnuroglu@gmail.com>
2022-10-20 08:26:25 +0300

    minor: login endpoint

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -2,7 +2,7 @@ import { LoginRequest, LoginResponse } from "../models";
 import { AxiosService } from "./axios.service";
 
 const login = (payload: LoginRequest): Promise<LoginResponse> => {
-  let result$ = AxiosService.post<LoginResponse>(`users/login`, payload);
+  let result$ = AxiosService.post<LoginResponse>(`auth/login`, payload);
   return result$;
 };
 
